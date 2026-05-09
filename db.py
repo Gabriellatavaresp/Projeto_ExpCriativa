df5a0d7f1de39cbc5a82bf8835cfcdb14ee6ea0e
João Lucas <joaolucas16004@gmail.com>
2026-05-09 17:26:43 -0300

    feat: redesign da interface de login com estilização customizada e atualização da tipografia do site e configurações de segurança

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ DB_CONFIG = {
     "host": "127.0.0.1",
     "port": 3306,
     "user": "root",
-    "password": "PUC@1234",
+    "password": "Puc@12345A",
     "database": "AuroraStreaming", 
     "cursorclass": pymysql.cursors.DictCursor
 }
